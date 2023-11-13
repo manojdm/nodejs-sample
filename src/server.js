59778a085e12ffb79b5149fa1f67728092929674
manojdm <manumanoj709@gmail.com>
2023-11-13 20:59:29 +0530

    chore: adding hello world route

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (req, res) => {
+	res.send("Hello World!");
+});
+
 app.listen("5500", () => {
 	console.log("Server is running...");
 });
